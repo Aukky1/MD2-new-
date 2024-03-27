120afd0124598dfa3e07a1971941cb06da825792
Thitiphat <thitiphat.tana@kmutt.ac.th>
2024-03-27 23:06:23 +0700

    second commit

--- main.js
+++ main.js
@@ -1,40 +1,30 @@
-let totalName = 0
 function init() {  
-    const addButton = document.getElementById('addButton')  
-    addButton.addEventListener('click', showName)
+    const addButton = document.getElementById("addButton");  
+    addButton.addEventListener("click", showName);
 }
-    function showName() {  
-        const inputName = document.getElementById('yourName')  
-        //   console.log(inputName.value)  
-        const divShowName = document.getElementById('showName')  
-        //    <div></div>  
-        const divElement = document.createElement('div')  
-        divElement.style = 'display:flex'  
-        //create checkbox and add to div  
-        const checkboxElement = document.createElement('input') 
-        //<input/>  
-        checkboxElement.setAttribute('type', 'checkbox') 
-        //<input type='checkbox'/>  
-        divElement.appendChild(checkboxElement)  
-        const totalNameElement = document.getElementById('totalName')  
-        //add click event to checkbox for removing  
-        checkboxElement.addEventListener('click', () => {    
-            divElement.remove()    
-            totalName--    
-            totalNameElement.textContent = totalName  
-        })  
-            // create <p>Your input name<p> and add to div  
-            const pElement = document.createElement('p') 
-             pElement.textContent = inputName.value  
-             //<div> <p>Your input name<p></div>  
-             divElement.appendChild(pElement)  
-             // <div id="showName">  
-             //    <div><p>Your input name<p></div>  
-             // </div>  
-             //   divShowName.textContent = ''  
-             divShowName.appendChild(divElement)  
-             inputName.value = ''  
-             totalName++  
-             totalNameElement.textContent = totalName
-            }
-             init()
+function showName() {  
+    const inputName = document.getElementById("yourName"); 
+    const divShowName = document.getElementById("showName");  
+    const divElement = document.createElement("div");  
+    divElement.style = "display:flex";  
+    const checkboxElement = document.createElement("input");  
+    checkboxElement.setAttribute("type", "checkbox");  
+    divElement.appendChild(checkboxElement);  
+    checkboxElement.addEventListener("click", () => {    
+        divElement.remove();    countCheckboxElement();  
+    });  
+    const pElement = document.createElement("p");  
+    pElement.textContent = inputName.value;  
+    divElement.appendChild(pElement);  
+    divShowName.appendChild(divElement);  
+    inputName.value = "";  
+    countCheckboxElement();
+}
+    function countCheckboxElement() {  
+        let showNamediv = document.getElementById("showName");  
+        let checkboxes = showNamediv.querySelectorAll('input[type="checkbox"]');  
+        let checkboxCount = checkboxes.length;  
+        let totalName = document.getElementById("totalName");  
+        totalName.textContent = checkboxCount;
+    }
+    init();
